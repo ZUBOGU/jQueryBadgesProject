bf6b3fbb6703d355b05199a0f6869f1ddc5e6b35
Zubo Gu <gu.zubo@berkeley.edu>
2018-01-15 16:34:19 -0800

    initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax('https://www.codeschool.com/users/3302389.json', {
+  	dataType: 'jsonp',
+  	success: function(response) {
+  		for (var e in response.courses.completed) {
+  			$('#badges').append('<div class="course">' + e + '</div>');
+  		}
+  		$('.course').append('<h3>' + $('.course').closest('title') + '</h3'>);
+  		$('.course').append('<img src=' + $('.course').closest('badge') + '>');
+  		$('.course').append('<a href=' +$('.course').closest('url') +  'target = "_blank"></a>');
+  	}
+  });
 });
